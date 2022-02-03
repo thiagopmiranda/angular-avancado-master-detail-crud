569b309f121e6f526f60fd015ed74f7e7c07ae0f
Thiago Miranda <thiagopereira99@gmail.com>
2022-02-03 11:14:04 -0300

    adding in-memory-database for categories

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.css']
+  styleUrls: ['./category-list.component.css'],
 })
 export class CategoryListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  alert() {
+    alert('Excluir');
   }
-
 }
