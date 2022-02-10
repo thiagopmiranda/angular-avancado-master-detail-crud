d56a45859815b63358a99138b33ca24f58307ac5
Thiago Miranda <thiagopereira99@gmail.com>
2022-02-10 15:01:46 -0300

    improving EntryService with BaseResourceService

--- src/app/pages/entries/shared/entry.model.ts
+++ src/app/pages/entries/shared/entry.model.ts
@@ -3,7 +3,7 @@ import { Category } from '../../categories/shared/category.model';
 
 export class Entry extends BaseResourceModel {
   constructor(
-    public id?: number,
+    public override id?: number,
     public name?: string,
     public description?: string,
     public type?: string,
