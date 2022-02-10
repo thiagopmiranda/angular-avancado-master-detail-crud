c0010c97f0b96fa9c7bc692bcb459bbc079afdfe
Thiago Miranda <thiagopereira99@gmail.com>
2022-02-10 10:08:44 -0300

    project restructuring

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+
+import { SharedModule } from '../../shared/shared.module';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
@@ -8,6 +8,6 @@ import { CategoryFormComponent } from './category-form/category-form.component';
 
 @NgModule({
   declarations: [CategoryListComponent, CategoryFormComponent],
-  imports: [CommonModule, CategoriesRoutingModule, ReactiveFormsModule],
+  imports: [SharedModule, CategoriesRoutingModule],
 })
 export class CategoriesModule {}
--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../../shared/shared.module';
 
 import { EntriesRoutingModule } from './entries-routing.module';
 
@@ -12,12 +11,6 @@ import { IMaskModule } from 'angular-imask';
 
 @NgModule({
   declarations: [EntryListComponent, EntryFormComponent],
-  imports: [
-    CommonModule,
-    EntriesRoutingModule,
-    ReactiveFormsModule,
-    CalendarModule,
-    IMaskModule,
-  ],
+  imports: [SharedModule, EntriesRoutingModule, CalendarModule, IMaskModule],
 })
 export class EntriesModule {}
