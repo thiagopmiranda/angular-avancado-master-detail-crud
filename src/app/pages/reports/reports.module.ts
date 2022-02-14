2fa3f9da4189a2b7a9bcfad8c421a6962108f8e6
Thiago Miranda <thiagopereira99@gmail.com>
2022-02-14 10:01:56 -0300

    improving Reports

--- src/app/pages/reports/reports.module.ts
+++ src/app/pages/reports/reports.module.ts
@@ -4,8 +4,10 @@ import { SharedModule } from '../../shared/shared.module';
 import { ReportsRoutingModule } from './reports-routing.module';
 import { ReportsComponent } from './reports/reports.component';
 
+import { ChartModule } from 'primeng/chart';
+
 @NgModule({
   declarations: [ReportsComponent],
-  imports: [SharedModule, ReportsRoutingModule],
+  imports: [SharedModule, ReportsRoutingModule, ChartModule],
 })
 export class ReportsModule {}
